d05e3be82aac65b19df525144c9463560bbd9e1d
Kirubel Tadesse <kirubel.m.tadesse@gmail.com>
2021-11-19 16:53:07 -0500

    Development (#9)
    add .gitattributes file to fix end line character conflict

--- install.sh
+++ install.sh
@@ -7,12 +7,19 @@
 # TODO: simple if/else block
 # check if the folder is exist
 # check if the dot folder exist 
+
 # mv ~/dotfiles ~/.dotfiles 																					# making it a dot folder
+
 
 # creat the symlinks
 # ln -sv "~/.dotfiles/runcom/.bash_profile" ~
 # ln -sv "~/.dotfiles/runcom/.inputrc" ~
-ln -sv "~/.dotfiles/runcom/.vim/.vimrc" $HOME/
-ln -sv "~/.dotfiles/runcom/.vim" $HOME/
-ln -sv "~/.dotfiles/runcom/.tmux.conf" $HOME/
+
+
+ln -s "$HOME/.dotfiles/runcom/.vim/.vimrc" $HOME/
+ln -s "$HOME/.dotfiles/runcom/.vim" $HOME/
+ln -s "$HOME/.dotfiles/runcom/.tmux.conf" $HOME/
+
 # ln -sv "~/.dotfiles/git/.gitconfig" $HOME/
+
+
