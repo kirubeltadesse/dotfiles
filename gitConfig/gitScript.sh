b6b3f7ce635cb2827063072c6c044bf5dc45085b
Kirubel Tadesse <kirubel.m.tadesse@gmail.com>
2022-09-14 12:45:55 -0400

    add git config bash script (#28)
    * tmux vim shortcut (#27)
    
    * add set paste in vimrc and settings.json
    
    * fixes #2  and added vim-airline
    
    * modify home path
    
    * .gitignore fix
    
    * add alias for git cherry-pick
    
    * fix gitignore
    
    * fix CRLF adn LF file endline problems
    
    * fix vimrc
    
    * fix coloring issues with tmux
    
    * fix Vudle installation problem
    
    * fix #7
    
    * fix #3
    
    * add vim copy-mode to tmux
    
    * change C-b to C-a binding in tmux
    
    * add docker alias
    
    * save files before refreshing line endings
    
    * normalize all the line endings
    
    * fix end of line
    
    * fix end of line to lf
    
    * fix commend not found error
    
    * add dos2unixd function
    
    * set vim for unix shell
    
    * fix .gitattributes
    
    * fix vscode/README.md
    
    * ignore log files
    
    * add vim search highlights
    
    * add kill-session for tmux
    
    * add git stash aliases
    
    * reorder the list of alias
    
    * enable alias for root user and update vscode extensions
    
    * add vscode keybindings.json settings.json with installer
    
    * add question for git status is slow in WSL2
    
    * add the solution for sharing  file
    
    * update the tmux short cut and organize vscode
    
    * fix #24
    
    * fixes #2  and added vim-airline
    
    * fix gitignore
    
    * fix CRLF adn LF file endline problems
    
    * fix merge conflict
    
    * add vim search highlights
    
    * add configuration shell script

---
+++ gitConfig/gitScript.sh
@@ -0,0 +1,9 @@
+# creating alias for git 
+
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.logdog 'log --decorate --oneline --graph'
+git config --global alias.sno 'show --name-only'
+
+# setting vim for git tool
+git config --global diff.tool vimdiff
+git config --global merge.tool vimdiff
