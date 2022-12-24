2b84bda78cfb8c10406c86c03162bae21e23b542
Kirubel Tadesse <ktadesse1@bloomberg.net>
2022-12-23 20:09:50 -0500

    add git alias install

--- gitConfig/gitScript.sh
+++ gitConfig/gitScript.sh
@@ -1,5 +1,6 @@
 # creating alias for git 
 
+echo "setup up git shortcuts"
 git config --global alias.last 'log -1 HEAD'
 git config --global alias.logdog 'log --decorate --oneline --graph'
 git config --global alias.sno 'show --name-only'
