adc7b68659a2720e17f84a175e3b82b392b11ff1
Kirubel Tadesse <ktadesse1@bloomberg.net>
2023-01-03 12:32:54 -0500

    add different gitdiff based on os

--- gitConfig/gitScript.sh
+++ gitConfig/gitScript.sh
@@ -8,6 +8,20 @@ git config --global alias.sno 'show --name-only'
 # TODO: add this information
 # git config --global alias.logauth 'log --author= {name}'
 
-# setting vim for git tool
-git config --global diff.tool vimdiff
-git config --global merge.tool vimdiff
+
+echo "check the os"
+
+# Get the name 
+os=$(uname)
+
+# FIXME: add windows os 
+if[ "$os" == "Darwin" ]; then
+	# setting vim for git tool
+ 	git config --global diff.tool vimdiff
+	git config --global merge.tool vimdiff
+else
+	echo "setting git diff for notebook"
+	git-nbdiffdriver config --enable --global
+	git-nbdifftool config --enable
+fi
+
