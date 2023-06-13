6348e2ddf12d7301606d4a71f9488dc5c25409e1
Kirubel Tadesse <ktadesse1@bloomberg.net>
2023-06-13 16:40:10 -0400

    update gitScript.sh and alias files

--- gitConfig/gitScript.sh
+++ gitConfig/gitScript.sh
@@ -1,13 +1,32 @@
 # creating alias for git 
 
 echo "setup up git shortcuts"
+
+# this is more formal way to create the git alias
+# https://git-scm.com/book/en/v2/Git-Basics-Git-Aliases
+
+git config --global alias.a add 
+git config --global alias.co checkout 
+git config --global alias.b branch
+git config --global alias.c commit 
+git config --global alias.cp cherry-pick
+git config --global alias.d diff 
 git config --global alias.last 'log -1 HEAD'
+git config --global alias.logauth 'log --author'
 git config --global alias.logdog 'log --decorate --oneline --graph'
+git config --global alias.m merge 
+git config --global alias.ps push 
+git config --global alias.pl pull 
+git config --global alias.r remote 
+git config --global alias.sa 'stash apply' 
+git config --global alias.sc 'stash clear' 
+git config --global alias.sd 'stash drop' 
+git config --global alias.sl 'stash list'
+git config --global alias.sp 'stash pop' 
+git config --global alias.ss 'stash save' 
+git config --global alias.st status 
+git config --global alias.sw 'stash show' 
 git config --global alias.sno 'show --name-only'
-
-# TODO: add this information
-# git config --global alias.logauth 'log --author= {name}'
-
 
 # Get the name 
 os=$(uname)
--- runcom/.bash_profile
+++ runcom/.bash_profile
@@ -44,6 +44,8 @@ function kill_socat() {
     done
 }
 
+## FIXME: this might break fzf tab complitions
+## [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
 
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="$HOME/.sdkman"
