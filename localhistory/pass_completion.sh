2a16528b9d4cd233cd4eb597a6ffdf871ffa6508
Kirubel Tadesse <ktadesse1@bloomberg.net>
2024-03-27 10:42:23 +0000

    fix -bash.compopt: command not found error for pass complete

--- localhistory/pass_completion.sh
+++ localhistory/pass_completion.sh
@@ -3,7 +3,7 @@
 # Copyright (C) 2012 - 2014 Jason A. Donenfeld <Jason@zx2c4.com> and
 # Brian Mattern <rephorm@rephorm.com>. All Rights Reserved.
 # This file is licensed under the GPLv2+. Please see COPYING for more information.
-_pass_complete_entries () {
+_pass_complete_entries() {
         local prefix="${PASSWORD_STORE_DIR:-$HOME/.password-store/}"
         prefix="${prefix%/}/"
         local suffix=".gpg"
@@ -26,10 +26,10 @@ _pass_complete_entries () {
                 if [[ ${#items[@]} -eq 1 && $autoexpand -eq 1 ]]; then
                         while [[ -d $item ]]; do
                                 local subitems=($(compgen -f "$item/"))
-                                local filtereditems=( ) item2
+                                local filtereditems=() item2
                                 for item2 in "${subitems[@]}"; do
                                         [[ $item2 =~ /\.[^/]*$ ]] && continue
-                                        filtereditems+=( "$item2" )
+                                        filtereditems+=("$item2")
                                 done
                                 if [[ ${#filtereditems[@]} -eq 1 ]]; then
                                         item="${filtereditems[0]}"
@@ -37,7 +37,7 @@ _pass_complete_entries () {
                                         break
                                 fi
                         done
-				fi
+                fi
 
                 # append / to directories
                 [[ -d $item ]] && item="$item/"
@@ -52,12 +52,14 @@ _pass_complete_entries () {
 
         # The only time we want to add a space to the end is if there is only
         # one match, and it is not a directory
-        if [[ $i -gt 1 || ( $i -eq 1 && -d $firstitem ) ]]; then
-                compopt -o nospace
-        fi
+        # if [[ $i -gt 1 || ( $i -eq 1 && -d $firstitem ) ]]; then
+        # if [[ $i -eq 1 && ! -d $firstitem ]]; then
+        #         # compopt -o nospace
+        #         COMPREPLY+=(" ")
+        # fi
 }
 
-_pass_complete_folders () {
+_pass_complete_folders() {
         local prefix="${PASSWORD_STORE_DIR:-$HOME/.password-store/}"
         prefix="${prefix%/}/"
 
@@ -69,7 +71,7 @@ _pass_complete_folders () {
         done
 }
 
-_pass_complete_keys () {
+_pass_complete_keys() {
         local GPG="gpg"
         command -v gpg2 &>/dev/null && GPG="gpg2"
 
@@ -84,43 +86,43 @@ _pass() {
         local cur="${COMP_WORDS[COMP_CWORD]}"
         local commands="init ls find grep show insert generate edit rm mv cp git help version ${PASSWORD_STORE_EXTENSION_COMMANDS[*]}"
         if [[ $COMP_CWORD -gt 1 ]]; then
-                local lastarg="${COMP_WORDS[$COMP_CWORD-1]}"
+                local lastarg="${COMP_WORDS[$COMP_CWORD - 1]}"
                 case "${COMP_WORDS[1]}" in
-                        init)
-                                if [[ $lastarg == "-p" || $lastarg == "--path" ]]; then
-                                        _pass_complete_folders
-                                        compopt -o nospace
-                                else
-                                        COMPREPLY+=($(compgen -W "-p --path" -- ${cur}))
-                                        _pass_complete_keys
-                                fi
-                                ;;
-                        ls|list|edit)
-                                _pass_complete_entries
-                                ;;
-                        show|-*)
-                                COMPREPLY+=($(compgen -W "-c --clip" -- ${cur}))
-                                _pass_complete_entries 1
-                                ;;
-                        insert)
-                                COMPREPLY+=($(compgen -W "-e --echo -m --multiline -f --force" -- ${cur}))
-                                _pass_complete_entries
-                                ;;
-                        generate)
-                                COMPREPLY+=($(compgen -W "-n --no-symbols -c --clip -f --force -i --in-place" -- ${cur}))
-                                _pass_complete_entries
-                                ;;
-                        cp|copy|mv|rename)
-                                COMPREPLY+=($(compgen -W "-f --force" -- ${cur}))
-                                _pass_complete_entries
-                                ;;
-                        rm|remove|delete)
-                                COMPREPLY+=($(compgen -W "-r --recursive -f --force" -- ${cur}))
-                                _pass_complete_entries
-                                ;;
-                        git)
-                                COMPREPLY+=($(compgen -W "init push pull config log reflog rebase" -- ${cur}))
-                                ;;
+                init)
+                        if [[ $lastarg == "-p" || $lastarg == "--path" ]]; then
+                                _pass_complete_folders
+                                compopt -o nospace
+                        else
+                                COMPREPLY+=($(compgen -W "-p --path" -- ${cur}))
+                                _pass_complete_keys
+                        fi
+                        ;;
+                ls | list | edit)
+                        _pass_complete_entries
+                        ;;
+                show | -*)
+                        COMPREPLY+=($(compgen -W "-c --clip" -- ${cur}))
+                        _pass_complete_entries 1
+                        ;;
+                insert)
+                        COMPREPLY+=($(compgen -W "-e --echo -m --multiline -f --force" -- ${cur}))
+                        _pass_complete_entries
+                        ;;
+                generate)
+                        COMPREPLY+=($(compgen -W "-n --no-symbols -c --clip -f --force -i --in-place" -- ${cur}))
+                        _pass_complete_entries
+                        ;;
+                cp | copy | mv | rename)
+                        COMPREPLY+=($(compgen -W "-f --force" -- ${cur}))
+                        _pass_complete_entries
+                        ;;
+                rm | remove | delete)
+                        COMPREPLY+=($(compgen -W "-r --recursive -f --force" -- ${cur}))
+                        _pass_complete_entries
+                        ;;
+                git)
+                        COMPREPLY+=($(compgen -W "init push pull config log reflog rebase" -- ${cur}))
+                        ;;
                 esac
 
                 # To add completion for an extension command define a function like this:
@@ -130,7 +132,7 @@ _pass() {
                 # }
                 #
                 # and add the command to the $PASSWORD_STORE_EXTENSION_COMMANDS array
-                if [[ " ${PASSWORD_STORE_EXTENSION_COMMANDS[*]} " == *" ${COMP_WORDS[1]} "* ]] && type "__password_store_extension_complete_${COMP_WORDS[1]}" &> /dev/null; then
+                if [[ " ${PASSWORD_STORE_EXTENSION_COMMANDS[*]} " == *" ${COMP_WORDS[1]} "* ]] && type "__password_store_extension_complete_${COMP_WORDS[1]}" &>/dev/null; then
                         "__password_store_extension_complete_${COMP_WORDS[1]}"
                 fi
         else
@@ -138,5 +140,3 @@ _pass() {
                 _pass_complete_entries 1
         fi
 }
-
-
