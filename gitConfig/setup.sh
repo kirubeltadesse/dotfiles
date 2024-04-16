818f90ec3f10aa84aa3c41828737a47e8f56bcb1
Kirubel Tadesse <ktadesse1@bloomberg.net>
2024-04-16 17:43:17 +0100

    add setup script for delta

--- gitConfig/setup.sh
+++ gitConfig/setup.sh
@@ -134,6 +134,17 @@ if [[ "$os" == "Darwin" || "$os" == "Linux" || "$os" == "SunOS" ]]; then
 	git config --global merge.conflictstyle diff3
 	git config --global mergetool.prompt false
 	git config --global core.excludesFile "${HOME}/.dotfiles/gitConfig/gitignore"
+	# add delta configuration
+	git config --global core.pager delta 
+	git config --global interactive.diffFilter "delta --color-only --features=interactive"
+	git config --global delta.features decorations 
+	git config --global delta.interactive.keep-plus-minus-markers false
+	git config --global delta.decorations.commit-decoration-style "blue ol"
+	git config --global delta.decorations.hunk-header-decoration-style "blue box"
+	git config --global delta.decorations.hunk-header-file-style red
+	git config --global delta.decorations.hunk-header-style "file line-number syntax"
+	git config --global delta.decorations.hunk-header-line-number-style "#067a00"
+	
 else
 	echo "setting git diff for $1"
 	git config --global core.editor vi
