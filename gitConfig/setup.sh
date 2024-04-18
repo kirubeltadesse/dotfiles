d70325ad3dd1f99ef08a698a8a733b4d1ceffee8
Kirubel Tadesse <kirubel.m.tadesse@gmail.com>
2024-04-18 22:28:23 +0100

    add helper function and environment files to help the setup

--- gitConfig/setup.sh
+++ gitConfig/setup.sh
@@ -50,10 +50,9 @@ print warning "setup up git shortcuts"
 # this is more formal way to create the git alias
 # https://git-scm.com/book/en/v2/Git-Basics-Git-Aliases
 git config --global init.defaultBranch main
-# FIXME: get credential take from a local .env file
 git config --global credential.helper cache
-git config --global credential.https://github.com.email "kirubel.m.tadesse@gmail.com"
-git config --global credential.https://github.com.user kirubeltadesse
+git config --global credential.https://github.com.email "$(read_file "$USERNAME_FILE")"
+git config --global credential.https://github.com.user "$(read_file "$EMAIL_FILE")" 
 git config --global branch.autosetuprebase always
 git config --global alias.a add
 git config --global alias.co checkout
--- utility/utilities.sh
+++ utility/utilities.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DOTFILE_DIR="/tmp/dotfiles"
+
+# Store the user's custom info
+EMAIL_FILE="$DOTFILE_DIR/email.txt"
+USERNAME_FILE="$DOTFILE_DIR/username.txt"
+
 # Define the function to echo colored text
 print() {
 	local type="$1"
@@ -127,8 +134,8 @@ install_apps() {
 
 # Function to create symbolic links
 create_symlink() {
-	local source_file=$1
-	local target_file=$2
+	local source_file="$1"
+	local target_file="$2"
 
 	if [ ! -e "$target_file" ]; then
 		ln -s "$source_file" "$target_file"
@@ -140,7 +147,7 @@ create_symlink() {
 }
 
 copy_text_2_bashrc() {
-	local text=$1
+	local text="$1"
 	local file_path="$HOME/.bashrc"
 
 	while IFS= read -r line; do
@@ -149,7 +156,7 @@ copy_text_2_bashrc() {
 }
 
 copy_text_2_bashrc() {
-	local text=$1
+	local text="$1"
 	local file_path="$HOME/.bashrc"
 
 	while IFS= read -r line; do
@@ -158,18 +165,51 @@ copy_text_2_bashrc() {
 }
 
 configure_keybase() {
-	# TODO: import keys from keybase
 	keybase pgp export | gpg --import                                            # importing public key
 	keybase pgp export --secret | gpg --batch --import --allow-secret-key-import # importing private key
 }
 
 create_env_file() {
-	# TODO ask the use for the git-username
+	mkdir -p $DOTFILE_DIR
+
 	read -p "Enter git-username: " username
 	read -p "Enter git-eamil: " email
-	# TODO ask the use for the git-email
-	# create a file and write to the file
-	true
+	write_to_file "$username" "$USERNAME_FILE"
+	write_to_file "$email" "$EMAIL_FILE"
+	return
+}
+
+write_to_file() {
+	local data="$1"
+	local file="$2"
+	echo "$data" > "$file"
+}
+
+read_file() {
+	local file="$1"
+	local lines=()
+	if [ -f "$file" ]; then
+		while IFS= read -r line; do
+			lines+=("$line") # Store each line in an array
+		done < "$file"
+		echo "${lines[@]}" # Output the array content
+	else
+		echo 1
+	fi
+}
+
+clean_env() {
+	# TODO: not begin called anywhere at this point
+	remove_file "$USERNAME_FILE"
+	remove_file "$EMAIL_FILE"
+	rm -f $DOTFILE_DIR
+}
+
+remove_file() {
+	local file="$1"
+	if [ -f "$file" ]; then
+		rm "$file"
+	fi
 }
 
 delete_env_file() {
