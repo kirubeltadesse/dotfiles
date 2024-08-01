a65e52dd1fc100f6dedfabb23829126932a111ec
Kirubel Tadesse <ktadesse1@bloomberg.net>
2024-08-01 10:18:33 +0100

    fix: Update the none-ls for recent version

--- runcom/config/nvim/lua/plugins/none-ls.lua
+++ runcom/config/nvim/lua/plugins/none-ls.lua
@@ -1,15 +1,20 @@
 return {
     "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
     config = function()
         local null_ls = require("null-ls")
 
         null_ls.setup({
             sources = {
+                require("none-ls.diagnostics.cpplint"),
+                require("none-ls.diagnostics.eslint_d"),
+                require("none-ls.code_actions.eslint"),
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.prettier,
-                null_ls.builtins.diagnostics.eslint_d,
                 null_ls.builtins.formatting.black,
-                null_ls.builtins.formatting.isort,                -- TODO: format and linter for cpp file
+                null_ls.builtins.formatting.isort, -- TODO: format and linter for cpp file
             },
         })
 
