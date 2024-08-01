b191ea921a0f32df77031359c5349f7f1e5654f8
Kirubel Tadesse <ktadesse1@bloomberg.net>
2024-08-01 10:43:04 +0100

    Update the formating for the vim endings

--- runcom/config/nvim/lua/swarmies/set.lua
+++ runcom/config/nvim/lua/swarmies/set.lua
@@ -1,31 +1,30 @@
--- syntax enable 
+-- syntax enable
 -- vim.opt.inchighlight
 -- colorscheme morning
 -- filetype plugin on
 -- Information on the following setting can be found with
 -- :help set
 --
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 4                            	  				--	this is the level of auto indent, adjust to taste
+vim.opt.shiftwidth = 4 --	this is the level of auto indent, adjust to taste
 
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.ruler = true
 
-
-vim.opt.swapfile = false  
+vim.opt.swapfile = false
 
 vim.opt.backup = false -- used to highlight the searched word
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false 			                              			-- used to highlight the searched word
-vim.opt.incsearch = true 									-- used to highlight incremental in searched word
+vim.opt.hlsearch = false -- used to highlight the searched word
+vim.opt.incsearch = true -- used to highlight incremental in searched word
 
-vim.opt.scrolloff = 8 									-- make sure to have the cursor 8 line up/below unless first/lest
+vim.opt.scrolloff = 8 -- make sure to have the cursor 8 line up/below unless first/lest
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
@@ -35,8 +34,8 @@ vim.opt.colorcolumn = "80"
 
 vim.opt.termguicolors = true
 vim.opt.nu = true
-vim.opt.rnu = true                                    	  				-- set relative number
-vim.opt.visualbell = true		                              				-- turning of the beep sound on the text edit
+vim.opt.rnu = true -- set relative number
+vim.opt.visualbell = true -- turning of the beep sound on the text edit
 -- " Uncommon below to make screen not flash on error
 -- vim.opt.vb t_vb=""
 --vim.opt.nofixendofline                          	    			-- save the file without the end of line character
@@ -50,18 +49,18 @@ vim.opt.laststatus = 2
 -- vim.opt.paste = true                                   	  			-- Massing up with Telescope keep the proper formatting while pasting on vim"
 -- vim.opt.backspace=indent,eol,start					        		-- fix the backspace issues
 --
-vim.opt.spelllang = 'en_us'
+vim.opt.spelllang = "en_us"
 vim.opt.spell = true
 
--- vim.opt.listchars = {                                                   -- see hidden chars and their
---     tab = "| ",
---     eol = '𝆨',
---     trail = '.'
--- }
--- vim.o.list = true
--- vim.o.updatetime = 100
+vim.opt.listchars = {                                                   -- see hidden chars and their
+    tab = "| ",
+    eol = '𝆨',
+    trail = '.'
+}
+vim.o.list = true
+vim.o.updatetime = 100
 
-vim.opt.cursorline = true 								-- this code is to high light the current line on vim
+vim.opt.cursorline = true -- this code is to high light the current line on vim
 -- highlight CursorLine guibg=#303000 ctermbg=234
 
 -- FINDING FILES:
@@ -79,5 +78,3 @@ vim.opt.wildmenu = true
 
 -- THINGS TO CONSIDERS:
 -- - :b lets you auto complete any open buffer
-
-
