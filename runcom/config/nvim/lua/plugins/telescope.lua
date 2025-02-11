19110c01048d824f0e734946547250f7aa53f74a
Kirubel Tadesse <ktadesse1@bloomberg.net>
2025-02-11 18:30:33 +0000

    Add shortcuts to telescope and fix preview scroll

--- runcom/config/nvim/lua/plugins/telescope.lua
+++ runcom/config/nvim/lua/plugins/telescope.lua
@@ -8,7 +8,14 @@ return {
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = " Find Files" })
             vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Git Files Search" })
-            vim.keymap.set('n', '<leader>lg', builtin.live_grep, { desc = "Live Grep" })
+            vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = "Git [C]ommits" })
+            vim.keymap.set('n', '<leader>gcf', builtin.git_bcommits, { desc = "Git [C]ommits for current [F]ile" })
+            vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = "Git [B]ranch" })
+            vim.keymap.set('n', '<leader>gps', builtin.git_status, { desc = "Git [S]tatus" })
+            vim.keymap.set('n', '<leader>pd', builtin.diagnostics, { desc = "Search [D]iagnostics" })
+            vim.keymap.set('n', '<leader>pm', builtin.marks, { desc = "[P]review [M]arks" })
+            vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = "[P]review [B]uffers" })
+            vim.keymap.set('n', '<leader>lg', builtin.live_grep, { desc = "[L]ive [G]rep" })
             vim.keymap.set('n', '<leader>pws', function()
                 local word = vim.fn.expand("<cword>")
                 builtin.grep_string({ search = word });
@@ -29,6 +36,18 @@ return {
         -- This is your opts table
         config = function()
             require("telescope").setup({
+                defaults = {
+                    mappings = {
+                        i = { -- Insert mode
+                            ["<M-j>"] = require("telescope.actions").preview_scrolling_down, -- Alt + j
+                            ["<M-k>"] = require("telescope.actions").preview_scrolling_up,  -- Alt + k
+                        },
+                        n = { -- Normal mode
+                            ["<M-j>"] = require("telescope.actions").preview_scrolling_down, -- Alt + j
+                            ["<M-k>"] = require("telescope.actions").preview_scrolling_up,  -- Alt + k
+                        }
+                    }
+                },
                 extensions = {
                     ["ui-select"] = {
                         require("telescope.themes").get_dropdown {
