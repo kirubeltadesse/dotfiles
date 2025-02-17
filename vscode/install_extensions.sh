0e63b9f57076abd8ec4a5eef4856afb0de82c104
Kirubel Tadesse <kirubel.m.tadesse@gmail.com>
2025-02-17 06:16:20 +0000

    lh configuration per project.  (#63)
    Add completion for lh script in addition to including swap, proxy, and localhist_add functions and refactor lh, pass and install script, and fix formatting and indentation issues. 
    
    * Fix bash formating with script.sh file
    
    * Refactor the lh and pass the installer
    
    * Change master to main in g rehom
    
    * Fix indentation on the output
    
    * Add completion for the script
    
    * Add set and unset proxy functionality
    
    * Uncompleted swap functionality
    
    * Update types
    
    * Add localhist_add funciton
    
    ---------
    
    Co-authored-by: Kirubel Tadesse <ktadesse1@bloomberg.net>
    Co-authored-by: kirubeltadesse <@bloomberg.net>

--- vscode/install_extensions.sh
+++ vscode/install_extensions.sh
@@ -3,7 +3,7 @@ echo "Installing Visual Studo Code extenstions"
 
 for extension in $(cat extensions); do
 	print warning "Installing or update : ${extension}"
-	code --install-extension $extension
+	code --install-extension "$extension"
 done
 
 # create a symlick file to get the update in sink instade of copying
@@ -13,8 +13,8 @@ print warning "--copy settings.json and keybindings.json for VScode"
 if [ "$os" == "Darwin" ]; then
 	print warning "setting loc for ${os}: "
 	# TODO: make smart update instead of direct swap
-	cp -f keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
-	cp -f settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+	cp -f keybindings.json "$HOME"/Library/Application\ Support/Code/User/keybindings.json
+	cp -f settings.json "$HOME"/Library/Application\ Support/Code/User/settings.json
 elif [ "$os" == "Linux" ]; then
 	print warning "setting loc for ${os}: "
 	cp -f keybindings.json /mnt/c/Users/kiyam/AppData/Roaming/Code/User/keybindings.json
