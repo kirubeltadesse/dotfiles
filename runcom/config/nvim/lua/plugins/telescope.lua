326b5b65d12234837c7183f9231d02c3b68b6139
Kirubel Tadesse <kirubel.m.tadesse@gmail.com>
2025-03-03 07:48:01 +0000

    Add nb searches to telescope

--- runcom/config/nvim/lua/plugins/telescope.lua
+++ runcom/config/nvim/lua/plugins/telescope.lua
@@ -6,16 +6,20 @@ return {
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             local builtin = require('telescope.builtin')
+            local nb_telescope = require('swarmies.telescope_nb')
             vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = " Find Files" })
             vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Git Files Search" })
             vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = "Git [C]ommits" })
             vim.keymap.set('n', '<leader>gcf', builtin.git_bcommits, { desc = "Git [C]ommits for current [F]ile" })
             vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = "Git [B]ranch" })
             vim.keymap.set('n', '<leader>gps', builtin.git_status, { desc = "Git [S]tatus" })
+            vim.keymap.set('n', '<leader>nb', nb_telescope.nb_find_files, { desc = "[N]ote[B]ooks Find Files" })
+            vim.keymap.set('n', '<leader>nbl', nb_telescope.nb_live_grep, { desc = "[N]ote[B]ooks [L]ive grap" })
+            vim.keymap.set('n', '<leader>lg', builtin.live_grep, { desc = "[L]ive [G]rep" })
             vim.keymap.set('n', '<leader>pd', builtin.diagnostics, { desc = "Search [D]iagnostics" })
             vim.keymap.set('n', '<leader>pm', builtin.marks, { desc = "[P]review [M]arks" })
             vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = "[P]review [B]uffers" })
-            vim.keymap.set('n', '<leader>lg', builtin.live_grep, { desc = "[L]ive [G]rep" })
+            vim.keymap.set('n', '<leader>ps', builtin.lsp_document_symbols, { desc = "[P]review [S]ymbols" })
             vim.keymap.set('n', '<leader>pws', function()
                 local word = vim.fn.expand("<cword>")
                 builtin.grep_string({ search = word });
@@ -24,7 +28,7 @@ return {
                 local word = vim.fn.expand("<cWORD>")
                 builtin.grep_string({ search = word });
             end, { desc = "Grep Current WORD" })
-            vim.keymap.set('n', '<leader>ps', function()
+            vim.keymap.set('n', '<leader>gs', function()
                 builtin.grep_string({ search = vim.fn.input("Grap > ") });
             end, { desc = "Grep Input" })
             vim.keymap.set('n', '<leader>vh', builtin.help_tags, { desc = "Vim Help Tags" })
