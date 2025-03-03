e4b46956359fe4487465499a4de29d0d699cd836
Kirubel Tadesse <ktadesse1@bloomberg.net>
2025-03-03 10:50:30 +0000

    Update keyboard shortcuts

--- runcom/config/nvim/lua/plugins/fugitive.lua
+++ runcom/config/nvim/lua/plugins/fugitive.lua
@@ -2,7 +2,7 @@ return {
     'tpope/vim-fugitive',
     event = 'VeryLazy',
     config = function()
-        vim.keymap.set("n", "<leader>g", vim.cmd.Git, { desc = "Git Fugitive" });
+        vim.keymap.set("n", "<C-g>", vim.cmd.Git, { desc = "Git Fugitive" });
         vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>", { desc = "get ours" });
         vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>", { desc = "get theirs" })
     end
