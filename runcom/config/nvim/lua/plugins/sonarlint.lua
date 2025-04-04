7d54f0d6b61da06c30896220edcb061023c3f2b8
Kirubel Tadesse <kirubel.m.tadesse@gmail.com>
2025-04-04 08:00:30 +0100

    Config sonarlint and analyzers

--- runcom/config/nvim/lua/plugins/sonarlint.lua
+++ runcom/config/nvim/lua/plugins/sonarlint.lua
@@ -1,4 +1,14 @@
 return {
-  -- "SonarSource/sonarlint-vscode",
-  "danarth/sonarlint.nvim", -- this is from gitlab
+    "danarth/sonarlint.nvim",
+    config = function()
+        local mason_path = require("mason-registry").get_package("sonarlint-language-server"):get_install_path()
+        require("sonarlint").setup({
+            server = {
+                cmd = { "java", "-jar", mason_path .. "/extension/server/sonarlint-ls.jar", '-stdio',
+                '-analyzers', mason_path .. "/extension/analyzers/sonarjava.jar" },
+                -- args = { "analyzar", "--format", "json", "--path", "$FILENAME" },
+            },
+            filetypes = { "java" },
+        })
+    end,
 }
