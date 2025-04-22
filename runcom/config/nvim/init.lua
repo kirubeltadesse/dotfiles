9707a2635a02cbbfec2170c73b9abad583866915
Kirubel Tadesse <kirubel.m.tadesse@gmail.com>
2025-04-22 04:53:10 +0100

    Add highlight on yank

--- runcom/config/nvim/init.lua
+++ runcom/config/nvim/init.lua
@@ -16,18 +16,30 @@ require("lazy").setup("plugins")
 vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('KirubelLspConfig', {}),
     callback = function(ev)
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Goto declaration", buffer = ev.buf })
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Goto definition", buffer = ev.buf })
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Go method definition", buffer = ev.buf })
-        vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol,
-            { desc = "Search Symobls Matching", buffer = ev.buf })
-        vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, { desc = "Diagnostic open float", buffer = ev.buf })
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action", buffer = ev.buf })
-        vim.keymap.set("n", "<leader>gr", require("telescope.builtin").lsp_references, { desc = "Go to references", buffer = ev.buf })
-        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Buffer rename", buffer = ev.buf })
-        vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { desc = "Help signature", buffer = ev.buf })
+        local map = function(key, func, desc)
+            vim.keymap.set("n", key, func, { desc = "LSP: " .. desc, buffer = ev.buf })
+        end
 
+        map("gD", vim.lsp.buf.declaration, "Goto declaration")
+        map("gd", vim.lsp.buf.definition, "Goto definition")
+        map("K", vim.lsp.buf.hover, "Go method definition")
+        map("<leader>vws", vim.lsp.buf.workspace_symbol,
+            "Search Symobls Matching")
+        map("<leader>vd", vim.diagnostic.open_float, "Diagnostic open float")
+        map("<leader>ca", vim.lsp.buf.code_action, "Code action")
+        map("<leader>gr", require("telescope.builtin").lsp_references, "Go to references")
+        map("<leader>r", vim.lsp.buf.rename, "Buffer rename")
+        map("<C-h>", vim.lsp.buf.signature_help, "Help signature")
     end
+})
+
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yenking (Copying) text",
+    group = vim.api.nvim_create_augroup("kirubelHighlightYank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 require("swarmies")
