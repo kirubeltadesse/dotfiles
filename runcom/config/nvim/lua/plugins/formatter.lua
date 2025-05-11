4176cae33c6ea159d7ac313ad4b7c7af3f9356f0
Kirubel Tadesse <ktadesse1@bloomberg.net>
2025-05-11 23:06:49 +0100

    Use mini.icons when possible

--- runcom/config/nvim/lua/plugins/formatter.lua
+++ runcom/config/nvim/lua/plugins/formatter.lua
@@ -1,38 +1,41 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
+	{ "tpope/vim-sleuth" },
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
 
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "isort", "black" },
-			javascript = { "prettierd", "prettier", stop_after_first = true },
-			typescript = { "prettierd", "prettier", stop_after_first = true },
-			css = { "prettier" },
-			json = { "prettier" },
-			yaml = { "prettier" },
-			markdown = { "prettier" },
-			cpp = { "clang_format" },
-		},
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				css = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				cpp = { "clang_format" },
+			},
 
-		format_on_save = {
-			timeout_ms = 500,
-			async = false,
-			lsp_format = "fallback",
+			format_on_save = {
+				timeout_ms = 500,
+				async = false,
+				lsp_format = "fallback",
+			},
 		},
-	},
-	keys = {
-		{
-			"<leader>ff",
-			function()
-				require("conform").format({
-					lsp_fallback = true,
-					async = false,
-					timeout_ms = 1000,
-				})
-			end,
-			desc = "LSP Format Buffer",
-			mode = { "n", "v" },
+		keys = {
+			{
+				"<leader>ff",
+				function()
+					require("conform").format({
+						lsp_fallback = true,
+						async = false,
+						timeout_ms = 1000,
+					})
+				end,
+				desc = "LSP Format Buffer",
+				mode = { "n", "v" },
+			},
 		},
 	},
 }
