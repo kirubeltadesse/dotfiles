02685ab8bacb72c326ec7cceece37e2a7aa7d858
Kirubel Tadesse <ktadesse1@bloomberg.net>
2025-05-18 22:09:24 +0100

    Add snack and mini trailspace
    - Removed fzf_nb and replaced it with nb_plug in using snacks picker

--- runcom/config/nvim/lua/plugins/sonarlint.lua
+++ runcom/config/nvim/lua/plugins/sonarlint.lua
@@ -2,21 +2,24 @@ return {
     "danarth/sonarlint.nvim",
     config = function()
         local mason_path = vim.fn.stdpath("data") .. "/mason/packages/sonarlint-language-server"
+        local lspconfig_util = require("lspconfig.util")
+        -- Find the project root using root_pattern
+        local root_dir = lspconfig_util.root_pattern(".sonarlint")(vim.fn.expand("%:p")) or vim.loop.cwd()
+
         require("sonarlint").setup({
             capabilities = require("blink.cmp").get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities()),
             server = {
-                -- name = "sonarlint",
                 cmd = {
                     "java",
                     "-Dsonar.verbose=true",
                     "-Dsonar.log.level=DEBUG",
+                    "-Dsonarlint.connect.mode.file=" .. root_dir .. "/.sonarlint/connectMode.json",
                     "-jar", mason_path .. "/extension/server/sonarlint-ls.jar",
                     "-stdio",
                     "-analyzers", mason_path .. "/extension/analyzers/sonarjava.jar",
                 },
             },
-            settings = {
-            },
+            settings = {},
             handlers = {
                 ["textDocument/codeAction"] = function(_, result, ctx, config)
                     if not result or vim.tbl_isempty(result) then
