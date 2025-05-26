811d7cb40dc9acc789bffd54382957f80094dd9d
Kirubel Tadesse <ktadesse1@bloomberg.net>
2025-05-26 12:55:03 +0100

    Add gh-dash to the workflow

--- runcom/config/nvim/lua/plugins/devicons.lua
+++ runcom/config/nvim/lua/plugins/devicons.lua
@@ -13,8 +13,6 @@ return {
 		"mskelton/termicons.nvim",
 		requires = { "nvim-tree/nvim-web-devicons" },
 		build = false,
-		config = function()
-			require("termicons").setup()
-		end,
+		opts = {},
 	},
 }
