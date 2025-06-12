9a7c1a66e77379a3b8bb6db3c03cfaeef4409179
Kirubel Tadesse <kirubel.m.tadesse@gmail.com>
2025-06-12 03:22:31 +0100

    Update iterm2 (#72)
    ## Pull Request Overview
    
    This PR updates the theme configurations for iTerm2 by introducing new Rosé Pine themes and adjustments to related configurations across vim, tmux, and GitHub Dash. Key changes include:
    - Adding and adjusting plugin and keymap configurations in the Neovim and tmux config files.
    - Introducing new Rosé Pine theme files (standard, Moon, and Dawn) along with updates to the documentation in the README.
    - Removing the previous Dracula license and updating key bindings in the GitHub Dash configuration.
    
    ### Reviewed Changes
    
    <details>
    <summary>Show a summary per file</summary>
    
    | File                                    | Description                                                                         |
    | --------------------------------------- | ----------------------------------------------------------------------------------- |
    | runcom/config/nvim/lua/plugins/vimux.lua | Adds an extra plugin and standardizes key mapping syntax.                           |
    | runcom/.tmux.conf                       | Updates tmux settings to use the rose-pine theme with additional customization.       |
    | iterm/rose-pine*.itermcolors             | Introduces new iTerm2 theme files for Rosé Pine (standard, Moon, Dawn).               |
    | iterm/README.md                         | Updates documentation and table of contents to reflect the new themes and usage info.  |
    | iterm/LICENSE                           | Removes existing MIT License text.                                                   |
    | gitConfig/gh-dash/config.yml             | Removes the open-in-browser key binding from the configuration.                      |
    </details>

--- runcom/config/nvim/lua/plugins/vimux.lua
+++ runcom/config/nvim/lua/plugins/vimux.lua
@@ -1,10 +1,13 @@
 return {
+  { "laktak/tome" },
+  {
     "preservim/vimux",
-    event= 'VeryLazy',
+    event = "VeryLazy",
     config = function()
-        vim.keymap.set('n', '<leader>vp', vim.cmd.VimuxPromptCommand, { desc = "[V]im [P]rompt", noremap = true })
-        vim.keymap.set('n', '<leader>vl', vim.cmd.VimuxRunLastCommand, { desc = "[V]im [L]ast", noremap = true })
-        vim.keymap.set('n', '<leader>vi', vim.cmd.VimuxInspectRunner, { desc = "[V]im [I]nspect", noremap = true })
-        vim.keymap.set('n', '<leader>vz', vim.cmd.VimuxZoomRunner, { desc = "[V]im [Z]oom", noremap = true })
-    end
+      vim.keymap.set("n", "<leader>vp", vim.cmd.VimuxPromptCommand, { desc = "[V]im [P]rompt", noremap = true })
+      vim.keymap.set("n", "<leader>vl", vim.cmd.VimuxRunLastCommand, { desc = "[V]im [L]ast", noremap = true })
+      vim.keymap.set("n", "<leader>vi", vim.cmd.VimuxInspectRunner, { desc = "[V]im [I]nspect", noremap = true })
+      vim.keymap.set("n", "<leader>vz", vim.cmd.VimuxZoomRunner, { desc = "[V]im [Z]oom", noremap = true })
+    end,
+  },
 }
