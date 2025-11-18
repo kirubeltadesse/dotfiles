b45c05c6d70b3ff6d37629524ee77de47b220fea
Kirubel Tadesse <ktadesse1@bloomberg.net>
2025-11-18 10:14:12 +0000

    Fix treesitter issue

--- runcom/config/nvim/lua/plugins/treesitter.lua
+++ runcom/config/nvim/lua/plugins/treesitter.lua
@@ -3,50 +3,77 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = "VeryLazy",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        ensure_installed = {
-          "bash",
-          "c",
-          "cpp",
-          "json",
-          "make",
-          "python",
-          "vim",
-          "vimdoc",
-          "lua",
-          "javascript",
-          "typescript",
-          "java",
+    main = "nvim-treesitter.configs",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "json",
+        "make",
+        "python",
+        "vim",
+        "vimdoc",
+        "lua",
+        "javascript",
+        "typescript",
+        "java",
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- disable = function(lang, buf)
+        --     if lang == 'markdown' then
+        --         return true
+        --     end
+        -- end,
+      },
+      indent = { enable = true },
+      folding = {
+        enable = true,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<Enter>", -- set to `false` to disable one of the mappings
+          node_incremental = "<Enter>",
+          scope_incremental = false,
+          node_decremental = "<Backspace>",
         },
-        auto_install = true,
-        highlight = {
+      },
+
+      textobjects = {
+        select = {
           enable = true,
-          -- disable = function(lang, buf)
-          --     if lang == 'markdown' then
-          --         return true
-          --     end
-          -- end,
-        },
-        indent = { enable = true },
-        folding = {
-          enable = true,
-        },
-        incremental_selection = {
-          enable = true,
+          lookahead = true,
           keymaps = {
-            init_selection = "<Enter>", -- set to `false` to disable one of the mappings
-            node_incremental = "<Enter>",
-            scope_incremental = false,
-            node_decremental = "<Backspace>",
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ao"] = "@comment.outer",
+            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+            ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
           },
+          selection_modes = {
+            ["@parameter.outer"] = "v",
+            ["@function.outer"] = "V",
+            ["@class.outer"] = "<c-v>",
+          },
+          include_surrounding_whitespace = true,
         },
-      })
-    end,
+        -- If you later want swap/move, add them here as well.
+      },
+    },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
   },
   {
     "nvim-treesitter/playground",
-    event = "VeryLazy",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    cmd = { "TSPlaygroundToggle", "TSHighlightCapturesUnderCursor" }, -- on-demand
+    keys = {
+      { "<leader>tp", "<cmd>TSPlaygroundToggle<cr>", desc = "TS Playground" },
+    },
   },
 }
